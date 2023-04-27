63ecb6a41d32c2947dd48e0b0f11b2d43562c6ba
Nikita Butnar <nick.butnar@jetrockets.com>
2023-04-27 13:06:44 +0400

    solution

--- arrays/factorial_sum/factorial_sum.js
+++ arrays/factorial_sum/factorial_sum.js
@@ -1,10 +1,14 @@
 function sumFactorial(arr) {
-  const elements = [];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let factorial = 1;
 
-  for (let i = 1; i <= arr; i++) {
-    elements.push(i);
+    for (let j = 1; j <= arr[i]; j++) {
+      factorial *= j;
+    }
+    sum += factorial;
   }
-  console.log(elements)
-}
+  return sum;
+  }
 
-console.log(sumFactorial([1, 2, 3, 4]));
+  console.log(sumFactorial([4, 6])) //744
