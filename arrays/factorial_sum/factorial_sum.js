e9f9cf5a207a9dde94bd7abb62f06509f4ef7e93
Nikita Butnar <nick.butnar@jetrockets.com>
2023-04-27 12:39:26 +0400

    array of passed number

---
+++ arrays/factorial_sum/factorial_sum.js
@@ -0,0 +1,10 @@
+function sumFactorial(arr) {
+  const elements = [];
+
+  for (let i = 1; i <= arr; i++) {
+    elements.push(i);
+  }
+  console.log(elements)
+}
+
+console.log(sumFactorial([1, 2, 3, 4]));
