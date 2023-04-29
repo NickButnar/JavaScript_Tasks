b581a6c5f318b0928dcdd4aa72ee19f8d1b8872e
Nikita Butnar <nick.butnar@jetrockets.com>
2023-04-30 02:41:00 +0400

    grouping in string solution

--- arrays/grouping_in_string/grouping_in_string.js
+++ arrays/grouping_in_string/grouping_in_string.js
@@ -1,20 +1,10 @@
 const isConsecutive = (str) => {
-  const strArray = str.split("");
-  console.log(strArray);
-
-  if (strArray.length < 2) {
-    return false;
-  }
-
-  for (let i = 0; i < strArray.length - 1; i++) {
-    if (strArray[i] === strArray[i + 1]) {
-      return true;
-    }
-    if (strArray[i] !== strArray[i + 1]) {
-      return false
+  for(let i = 0; i < str.length; i++){
+    if (str.slice(i).indexOf(str[i - 1]) > 0) {
+      return false;
     }
   }
+  return true;
 }
-
-console.log(isConsecutive("112233"));
-console.log(isConsecutive("11223311"));
+console.log(isConsecutive("112233")); // true
+console.log(isConsecutive("11223311")); // false
